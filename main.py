b24230037020b5fdc3a89c17d17c0e90fa796654
Illa821649451 <illapalamarcuk78@gmail.com>
2023-05-20 10:44:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,57 @@
+import warnings
+# print(f"NameError - {type(NameError)} - {issubclass(NameError, BaseException)}")
+#
+# try:
+#     print("start")
+#     print(10/0)
+#     print("No errors")
+# except NameError:
+#     print("We have an error")
+# except ZeroDivisionError:
+#     print("We have an ZeroDivisionError")
+
+# try:
+#     print("start")
+# except NameError as error:
+#     print(error)
+# else:
+#     print("No problems")
+#
+# finally:
+#     print("Finally")
+#
+# print("Next code")
+
+# def checker(a):
+#     if type(a) != str:
+#         raise TypeError(f"Sorry, we can`t work with {type(a)}," f"we need class str")
+#     else:
+#         return a
+#
+#
+# var = 10
+# checker(var)
+
+
+# class BuildingError(Exception):
+#     def __str__(self):
+#         return f"With so much material the house cannot be built"
+#
+# def check_material(amount_of_material,limit_value):
+#     if amount_of_material > limit_value:
+#         return 'enough material'
+#     else:
+#         raise BuildingError(amount_of_material)
+#
+# materials = 123
+# print(check_material(materials, 300))
+
+# warnings.simplefilter('ignore', SyntaxWarning)
+# warnings.simplefilter('error', ImportWarning)
+#
+# warnings.warn('Warning, no code here', SyntaxError)
+# try:
+#     warnings.warn('Warning, module not import', SyntaxError)
+# except:
+#     print("warning processed")
 
