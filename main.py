3d04c26c7377c3092447fb33d332e4c380957bf4
Illa821649451 <illapalamarcuk78@gmail.com>
2023-06-09 20:43:03 +0300

    1

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+result = []
 
+
+def divider(a, b):
+    try:
+        if a < b:
+            raise ValueError
+        if b > 100:
+            raise IndexError
+        return a / b
+    except ValueError:
+        print("Помилка: a < b")
+    except IndexError:
+        print("Помилка: b > 100")
+    except Exception as e:
+        print("Інша помилка:", type(e).__name__)
+
+
+data = {10: 2, 2: 5, "123": 4, 18: 0, (): 15, 8: 4}
+
+for key in data:
+    res = divider(key, data[key])
+    result.append(res)
+
+print(result)
